d104a42a1bcd658538db9403f3747d09dc5d3da6
gerlinp <gerlin.pierrelouis@gmail.com>
2021-11-23 23:57:47 -0500

    testing

---
+++ script.js
@@ -0,0 +1,17 @@
+function isIsogram(str){
+    str = str.toLowerCase();
+    let arr = str.split('');
+    let match = []
+    
+    if (match.length = 0 ) {
+      arr.forEach(letter => {
+        if (letter == arr[i]) {
+          match.push(letter)
+        }
+      })
+    }
+   }
+   
+  isIsogram("Dermatoglyphics")
+  isIsogram("aba")
+  isIsogram("moOse")
